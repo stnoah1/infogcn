65408f8fa5fee786744b1f2e251677020a742ff0
stnoah1@gmail.com <stnoah1@gmail.com>
2021-09-28 17:56:58 +0000

    add model R

--- model/baseline.py
+++ model/baseline.py
@@ -582,3 +582,60 @@ class ModelwP(nn.Module):
         x = self.drop_out(x)
 
         return self.fc(x)
+
+
+class ModelwR(nn.Module):
+    def __init__(self, num_class=60, num_point=25, num_person=2, graph=None, in_channels=3,
+                 drop_out=0, adaptive=True, num_set=3):
+        super(ModelwR, self).__init__()
+
+        A = np.stack([np.eye(num_point)] * num_set, axis=0)
+        self.num_class = num_class
+        self.num_point = num_point
+        self.data_bn = nn.BatchNorm1d(num_person * in_channels * num_point)
+        self.trans_mat = nn.Parameter(torch.eye(num_point))
+        base_channel = 64
+
+        self.l1 = TCN_aGCN_unit(in_channels, base_channel, A, residual=False, adaptive=adaptive)
+        self.l2 = TCN_aGCN_unit(base_channel, base_channel, A, adaptive=adaptive)
+        self.l3 = TCN_aGCN_unit(base_channel, base_channel, A, adaptive=adaptive)
+        self.l4 = TCN_aGCN_unit(base_channel, base_channel, A, adaptive=adaptive)
+        self.l5 = TCN_aGCN_unit(base_channel, base_channel*2, A, stride=2, adaptive=adaptive)
+        self.l6 = TCN_aGCN_unit(base_channel*2, base_channel*2, A, adaptive=adaptive)
+        self.l7 = TCN_aGCN_unit(base_channel*2, base_channel*2, A, adaptive=adaptive)
+        self.l8 = TCN_aGCN_unit(base_channel*2, base_channel*4, A, stride=2, adaptive=adaptive)
+        self.l9 = TCN_aGCN_unit(base_channel*4, base_channel*4, A, adaptive=adaptive)
+        self.l10 = TCN_aGCN_unit(base_channel*4, base_channel*4, A, adaptive=adaptive)
+        self.fc = nn.Linear(base_channel*4, num_class)
+        nn.init.normal_(self.fc.weight, 0, math.sqrt(2. / num_class))
+        bn_init(self.data_bn, 1)
+        if drop_out:
+            self.drop_out = nn.Dropout(drop_out)
+        else:
+            self.drop_out = lambda x: x
+
+    def forward(self, x):
+        N, C, T, V, M = x.size()
+        x = rearrange(x, 'n c t v m -> (n m t) v c')
+        x = self.trans_mat @ x
+        x = rearrange(x, '(n m t) v c -> n (m v c) t', n=N, m=M, t=T)
+        x = self.data_bn(x)
+        x = x.view(N, M, V, C, T).permute(0, 1, 3, 4, 2).contiguous().view(N * M, C, T, V)
+        x = self.l1(x)
+        x = self.l2(x)
+        x = self.l3(x)
+        x = self.l4(x)
+        x = self.l5(x)
+        x = self.l6(x)
+        x = self.l7(x)
+        x = self.l8(x)
+        x = self.l9(x)
+        x = self.l10(x)
+
+        # N*M,C,T,V
+        c_new = x.size(1)
+        x = x.view(N, M, c_new, -1)
+        x = x.mean(3).mean(1)
+        x = self.drop_out(x)
+
+        return self.fc(x)
