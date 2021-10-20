9aa1ddd007343f55931707888f62d130d5208c35
hg <stnoah1@gmail.com>
2021-10-20 20:59:05 +0000

    update ensemble

--- ensemble.py
+++ ensemble.py
@@ -13,15 +13,19 @@ if __name__ == "__main__":
                         help='the work folder for storing results')
     parser.add_argument('--alpha',
                         default=1,
+                        nargs='+',
                         help='weighted summation',
                         type=float)
 
-    parser.add_argument('--joint-dir',
+    parser.add_argument('--num-class',
+                        default=1,
+                        help='number of class in dataset',
+                        type=int)
+
+    parser.add_argument('--ckpt-dirs',
+                        nargs='+',
                         help='Directory containing "epoch1_test_score.pkl" for joint eval results')
-    parser.add_argument('--bone-dir',
-                        help='Directory containing "epoch1_test_score.pkl" for bone eval results')
-    parser.add_argument('--joint-motion-dir', default=None)
-    parser.add_argument('--bone-motion-dir', default=None)
+
 
     arg = parser.parse_args()
 
@@ -35,78 +39,40 @@ if __name__ == "__main__":
                 label.append(int(info['label']) - 1)
     elif 'ntu120' in arg.dataset:
         if 'xsub' in arg.dataset:
-            npz_data = np.load('./data/' + 'ntu120/' + 'NTU120_CSub.npz')
+            npz_data = np.load('./data/ntu120/CSub_aligned.npz')
             label = np.where(npz_data['y_test'] > 0)[1]
         elif 'xset' in arg.dataset:
-            npz_data = np.load('./data/' + 'ntu120/' + 'NTU120_CSet.npz')
+            npz_data = np.load('./data/ntu120/CSet_aligend.npz')
             label = np.where(npz_data['y_test'] > 0)[1]
     elif 'ntu' in arg.dataset:
         if 'xsub' in arg.dataset:
-            npz_data = np.load('./data/' + 'ntu/' + 'NTU60_CS.npz')
+            npz_data = np.load('./data/ntu/CS_aligned.npz')
             label = np.where(npz_data['y_test'] > 0)[1]
         elif 'xview' in arg.dataset:
-            npz_data = np.load('./data/' + 'ntu/' + 'NTU60_CV.npz')
+            npz_data = np.load('./data/ntu/CV_aligned.npz')
             label = np.where(npz_data['y_test'] > 0)[1]
     else:
         raise NotImplementedError
 
-    with open(os.path.join(arg.joint_dir, 'epoch1_test_score.pkl'), 'rb') as r1:
-        r1 = list(pickle.load(r1).items())
-
-    with open(os.path.join(arg.bone_dir, 'epoch1_test_score.pkl'), 'rb') as r2:
-        r2 = list(pickle.load(r2).items())
-
-    if arg.joint_motion_dir is not None:
-        with open(os.path.join(arg.joint_motion_dir, 'epoch1_test_score.pkl'), 'rb') as r3:
-            r3 = list(pickle.load(r3).items())
-    if arg.bone_motion_dir is not None:
-        with open(os.path.join(arg.bone_motion_dir, 'epoch1_test_score.pkl'), 'rb') as r4:
-            r4 = list(pickle.load(r4).items())
+    ckpts = []
+    for ckpt_dir in arg.ckpt_dirs:
+        with open(os.path.join(ckpt_dir, 'epoch1_test_score.pkl'), 'rb') as f:
+            ckpts.append(list(pickle.load(f).items()))
 
     right_num = total_num = right_num_5 = 0
 
-    if arg.joint_motion_dir is not None and arg.bone_motion_dir is not None:
-        arg.alpha = [0.6, 0.6, 0.4, 0.4]
-        for i in tqdm(range(len(label))):
-            l = label[i]
-            _, r11 = r1[i]
-            _, r22 = r2[i]
-            _, r33 = r3[i]
-            _, r44 = r4[i]
-            r = r11 * arg.alpha[0] + r22 * arg.alpha[1] + r33 * arg.alpha[2] + r44 * arg.alpha[3]
-            rank_5 = r.argsort()[-5:]
-            right_num_5 += int(int(l) in rank_5)
-            r = np.argmax(r)
-            right_num += int(r == int(l))
-            total_num += 1
-        acc = right_num / total_num
-        acc5 = right_num_5 / total_num
-    elif arg.joint_motion_dir is not None and arg.bone_motion_dir is None:
-        arg.alpha = [0.6, 0.6, 0.4]
-        for i in tqdm(range(len(label))):
-            l = label[:, i]
-            _, r11 = r1[i]
-            _, r22 = r2[i]
-            _, r33 = r3[i]
-            r = r11 * arg.alpha[0] + r22 * arg.alpha[1] + r33 * arg.alpha[2]
-            rank_5 = r.argsort()[-5:]
-            right_num_5 += int(int(l) in rank_5)
-            r = np.argmax(r)
-            right_num += int(r == int(l))
-            total_num += 1
-        acc = right_num / total_num
-        acc5 = right_num_5 / total_num
-    else:
-        for i in tqdm(range(len(label))):
-            l = label[i]
-            _, r11 = r1[i]
-            _, r22 = r2[i]
-            r = r11 + r22 * arg.alpha
-            rank_5 = r.argsort()[-5:]
-            right_num_5 += int(int(l) in rank_5)
-            r = np.argmax(r)
-            right_num += int(r == int(l))
-            total_num += 1
+    for i in tqdm(range(len(label))):
+        l = label[i]
+        r = np.zeros(arg.num_class)
+        for alpha, ckpt in zip(arg.alpha, ckpts):
+            _, r11 = ckpt[i]
+            r += r11 * alpha
+
+        rank_5 = r.argsort()[-5:]
+        right_num_5 += int(int(l) in rank_5)
+        r = np.argmax(r)
+        right_num += int(r == int(l))
+        total_num += 1
         acc = right_num / total_num
         acc5 = right_num_5 / total_num
 
