42b95fc56520363235b33fd2e35a3280b8597d73
hg <stnoah1@gmail.com>
2021-10-24 03:16:05 +0000

    update ensemble files

--- ensemble.py
+++ ensemble.py
@@ -70,11 +70,11 @@ def ensemble_ntu60_cv():
     items = [
         ('./wandb/run-20211021_084222-343ssdgx/files/epoch1_test_score.pkl', 1.5), # 95.5%, modal_idx=7, pose, random_rot (x)
         ('./wandb/run-20211022_132321-25wce9mh/files/best_score.pkl', 1.5), # 95.5%, modal_idx=6, pose, ramdom_rot
-        ('./wandb/run-20211021_174320-3poxff45/files/best_score.pkl', 1.5), # 95.3%, model_idx=5, pose, random_rot (x)
+        # ('./wandb/run-20211021_174320-3poxff45/files/best_score.pkl', 1.5), # 95.3%, model_idx=5, pose, random_rot (x)
         ('./wandb/run-20211022_041136-1x63lf4o/files/best_score.pkl', 1.5), # 95.2%, modal_idx=0, pose, random_rot
         ('./wandb/run-20211021_195555-18yn6ma4/files/epoch1_test_score.pkl', 1), # 93.6%, modal_idx=7, vel, random_rot (x)
         ('./wandb/run-20211022_172012-3inzut0k/files/best_score.pkl', 1), #93.9%, modal_idx=6, vel, random_rot
-        ('./wandb/run-20211022_234021-32k7vxbc/files/best_score.pkl', 1), #94.0%, modal_idx=5, vel, random_rot
+        # ('./wandb/run-20211022_234021-32k7vxbc/files/best_score.pkl', 1), #94.0%, modal_idx=5, vel, random_rot
         ('./wandb/run-20211022_041134-j2k33m98/files/best_score.pkl', 1), # 94.2%, modal_idx=0, vel, random_rot
     ]
 
@@ -87,7 +87,11 @@ def ensemble_ntu120_cs():
 
 def ensemble_ntu120_cset(): # alpha = 1e-3
     items = [
-        ('./wandb/run-20211023_024235-1g4jwn6g/files/best_score.pkl', 1.5), # 86.4%, modal_idx=9, pose, random_rot
+        ('./wandb/run-20211023_055740-1d58qxoa/files/best_score.pkl', 1.5), # 88.2%, modal_idx=6, pose, random_rot
+        ('./wandb/run-20211023_024235-1g4jwn6g/files/best_score.pkl', 1.5), # 86.4%, modal_idx=0, pose, random_rot
+        ('./wandb/run-20211023_162158-3r8u9pf9/files/best_score.pkl', 1),   # 84.3%, modal_idx=7, vel, random_rot
+        # ('./wandb/run-20211023_162341-1iu7ck41/files/best_score.pkl', 1),   # 84.4%, modal_dix=6, vel, random_rot
+        ('./wandb/run-20211023_162359-23l8mw9k/files/best_score.pkl', 1),   # 84.4%, modal_dix=0, vel, random_rot
     ]
     ensemble('ntu120/xset', items)
 
