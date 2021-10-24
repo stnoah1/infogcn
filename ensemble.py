8be70afa4439fdf710c382e312eb4aba87718268
hg <stnoah1@gmail.com>
2021-10-24 00:40:13 +0000

    Add files for kinetics

--- ensemble.py
+++ ensemble.py
@@ -12,7 +12,7 @@ def ensemble(ds, items):
             npz_data = np.load('./data/ntu120/CSub_aligned.npz')
             label = np.where(npz_data['y_test'] > 0)[1]
         elif 'xset' in ds:
-            npz_data = np.load('./data/ntu120/CSet_aligend.npz')
+            npz_data = np.load('./data/ntu120/CSet_aligned.npz')
             label = np.where(npz_data['y_test'] > 0)[1]
     elif 'ntu' in ds:
         num_class=60
@@ -52,34 +52,52 @@ def ensemble(ds, items):
     print('Top1 Acc: {:.4f}%'.format(acc * 100))
     print('Top5 Acc: {:.4f}%'.format(acc5 * 100))
 
-
-def ensemble_ntu60_cv():
-    items = [
-        ('./wandb/run-20211021_084222-343ssdgx/files/epoch1_test_score.pkl', 1.5), # 95.5%, modal_idx=7, pose
-        ('./wandb/run-20211021_042946-1klfr5ej/files/epoch1_test_score.pkl', 1.5), # 95.0%, modal_idx=0, pose
-        ('./wandb/run-20211021_174320-3poxff45/files/best_score.pkl', 1.5), # 95.3%,  model_idx=5, pose
-        ('./wandb/run-20211021_195555-18yn6ma4/files/epoch1_test_score.pkl', 1), # 93.6%, modal_idx=7, vel
-        ('./wandb/run-20211021_134505-zkmgkh6u/files/epoch1_test_score.pkl', 1), # 94.0%, modal_idx=0, vel
-    ]
-
-    ensemble('ntu/xview', items)
-
 def ensemble_ntu60_cs():
     items = [
-        ('./wandb/run-20211021_152338-3tne14dx/files/best_score.pkl',        1.5), # 90.5%, modal_idx=7, pose, random_rot
-        ('./wandb/run-20211021_235149-2g8ul0tg/files/epoch1_test_score.pkl', 1.5), # 89.7 modal_idx=6, pose
+        ('./wandb/run-20211021_152338-3tne14dx/files/best_score.pkl', 1.5), # 90.5%, modal_idx=7, pose, random_rot
+        ('./wandb/run-20211022_041137-3ooojndm/files/best_score.pkl', 1.5), # 90.5%, modal_idx=6, pose, random_rot
+        # ('./wandb/run-20211022_090145-158l8k1u/files/best_score.pkl', 1.5), # 90.2%, modal_idx=5, pose, random_rot
         ('./wandb/run-20211021_042926-1ea2za8d/files/epoch1_test_score.pkl', 1.5), # 89.8%, modal_idx=0, pose, random_rot
-        ('./wandb/run-20211021_175515-3pr6bb8c/files/best_score.pkl', 1), # 88.8%, modal_idx=6, vel
-        ('./wandb/run-20211021_111209-1j072ad2/files/epoch1_test_score.pkl', 1), # 88.5%, modal_idx=5, vel
-        ('./wandb/run-20211021_135605-46q24qws/files/epoch1_test_score.pkl', 1), # 88.9%, modal_idx=0, vel, epoch=130
-        # ('./wandb/run-20211020_035202-2zjkchh8/files/epoch1_test_score.pkl', 1), # 90.2%, modal_idx=7, pose
-
+        ('./wandb/run-20211021_195035-1xd1m0iq/files/best_score.pkl', 1), # 88.6%, modal_idx=7, vel, random_rot
+        ('./wandb/run-20211022_111250-3heeg9w8/files/best_score.pkl', 1), # 88.7, modal_idx=6, vel, random_rot
+        # ('./wandb/run-20211021_111209-1j072ad2/files/epoch1_test_score.pkl', 1), # 88.5%, modal_idx=5, vel, random_rot(x)
+        ('./wandb/run-20211021_135605-46q24qws/files/epoch1_test_score.pkl', 1), # 88.9%, modal_idx=0, vel, random_rot(x)
     ]
 
     ensemble('ntu/xsub', items)
 
+def ensemble_ntu60_cv():
+    items = [
+        ('./wandb/run-20211021_084222-343ssdgx/files/epoch1_test_score.pkl', 1.5), # 95.5%, modal_idx=7, pose, random_rot (x)
+        ('./wandb/run-20211022_132321-25wce9mh/files/best_score.pkl', 1.5), # 95.5%, modal_idx=6, pose, ramdom_rot
+        ('./wandb/run-20211021_174320-3poxff45/files/best_score.pkl', 1.5), # 95.3%, model_idx=5, pose, random_rot (x)
+        ('./wandb/run-20211022_041136-1x63lf4o/files/best_score.pkl', 1.5), # 95.2%, modal_idx=0, pose, random_rot
+        ('./wandb/run-20211021_195555-18yn6ma4/files/epoch1_test_score.pkl', 1), # 93.6%, modal_idx=7, vel, random_rot (x)
+        ('./wandb/run-20211022_172012-3inzut0k/files/best_score.pkl', 1), #93.9%, modal_idx=6, vel, random_rot
+        ('./wandb/run-20211022_234021-32k7vxbc/files/best_score.pkl', 1), #94.0%, modal_idx=5, vel, random_rot
+        ('./wandb/run-20211022_041134-j2k33m98/files/best_score.pkl', 1), # 94.2%, modal_idx=0, vel, random_rot
+    ]
+
+    ensemble('ntu/xview', items)
+
+def ensemble_ntu120_cs():
+    items = [
+    ]
+    ensemble('ntu120/xsub', items)
+
+def ensemble_ntu120_cset(): # alpha = 1e-3
+    items = [
+        ('./wandb/run-20211023_024235-1g4jwn6g/files/best_score.pkl', 1.5), # 86.4%, modal_idx=9, pose, random_rot
+    ]
+    ensemble('ntu120/xset', items)
+
 if __name__ == "__main__":
-    print('NTU60 CV')
-    ensemble_ntu60_cv()
-    print('NTU60 CS')
+    print('NTU60 XSub')
     ensemble_ntu60_cs()
+    print('NTU60 XView')
+    ensemble_ntu60_cv()
+    # print('NTU60 XSub')
+    # ensemble_ntu120_cs()
+    print('NTU120 XSet')
+    ensemble_ntu120_cset()
+
